94d23a38b9227087daef406af17d56a8d42c6d69
lxing <lxing@stanford.edu>
2013-10-03 21:42:45 -0700

    delete test

--- SimulationTest.cpp
+++ SimulationTest.cpp
@@ -37,15 +37,22 @@ TEST(SimulationTest, Simple) {
   assert(tissues->at(0)->cells()==U32(1));
 }
 
-// Step 3. Call RUN_ALL_TESTS() in main().
-//
-// We do this by linking in src/gtest_main.cc file, which consists of
-// a main() function which calls RUN_ALL_TESTS() for us.
-//
-// This runs all the tests you've defined, prints the result, and
-// returns 0 if successful, or 1 otherwise.
-//
-// Did you notice that we didn't register the tests?  The
-// RUN_ALL_TESTS() macro magically knows about all the tests we
-// defined.  Isn't this convenient?
+TEST(SimulationTest, Delete) {
+  SimulationTester simulation;
+  simulation.TissueIs("TestTissue");
 
+  vector< Fwk::Ptr<Tissue> >* tissues = simulation.GetTissues();
+  assert(tissues->size() == 1);
+  assert(strcmp(tissues->at(0)->name().c_str(), "TestTissue")==0);
+
+  Cell::Coordinates coord;
+  coord.x = 0;
+  coord.y = 0;
+  coord.z = 0;
+  simulation.CellIs("TestTissue", Cell::tCell_, coord);
+
+  Cell::Ptr cell = tissues->at(0)->cell(coord);
+  cell->healthIs(Cell::infected_);
+  simulation.InfectedCellsDel("TestTissue");
+  assert(tissues->at(0)->cells()==U32(0));
+}
