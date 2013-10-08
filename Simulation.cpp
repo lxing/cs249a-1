091676e9b1745d96dd08cf79cfadcc32d3aeb80b
Lawrence Xing <lawrence.xing@gmail.com>
2013-10-08 10:13:23 -0700

    Reactor tracking

--- Simulation.cpp
+++ Simulation.cpp
@@ -18,9 +18,10 @@ void Simulation::TissueIs(Fwk::String _tissueName) {
   Tissue::Ptr tissue = GetTissue(_tissueName);
   if (tissue.ptr()) return;
   Tissue::Ptr newTissue(Tissue::TissueNew(_tissueName));   
-  TissueReactor::Ptr tReactor = TissueReactor::TissueReactorIs(newTissue);
-
   tissues_.push_back(newTissue); 
+
+  TissueReactor::Ptr reactor = TissueReactor::TissueReactorNew(newTissue);
+  reactors_.push_back(reactor);
 }
 
 void Simulation::CellIs(Fwk::String _tissueName, Cell::CellType _type, 
@@ -110,12 +111,12 @@ void Simulation::AntibodyStrengthIs (Fwk::String _tissueName, Cell::Coordinates 
 void Simulation::TissueReactor::onCellNew(Cell::Ptr _cell) {
   AntibodyStrength strength;
   switch (_cell->cellType()) {
-    case Cell::cytotoxicCell_ : strength.valueIs(100);
-    case Cell::helperCell_ : strength.valueIs(0);
+    case Cell::cytotoxicCell_ : strength.valueIs(100); break;
+    case Cell::helperCell_ : strength.valueIs(0); break;
     default : throw std::runtime_error("Invalid cell type");
   }
 
-  // Break abstraction to manually iteratre through sides
+  // Break abstraction to manually iterate through sides
   for (int i = SIDE_MIN; i < SIDE_MAX; i++) {
     CellMembrane::Side side = (CellMembrane::Side) i;
     CellMembrane::Ptr membrane = _cell->membraneNew(MEMBRANE_NAME, side);
@@ -132,24 +133,31 @@ Tissue::Ptr Simulation::GetTissue(Fwk::String _tissueName) {
   Tissue::Ptr tissue;
   for (TissueList::iterator it = tissues_.begin(); it != tissues_.end(); ++it) {
     tissue = *it;
-    if (!strcmp(tissue->name().c_str(), _tissueName.c_str())) {
-      return tissue;
-    }
+    if (!strcmp(tissue->name().c_str(), _tissueName.c_str()))
+      break;
   }
   return tissue;
 }
 
-
+Simulation::TissueReactor::Ptr Simulation::GetReactor(Fwk::String _tissueName) {
+  TissueReactor::Ptr reactor;
+  for (ReactorList::iterator it = reactors_.begin(); it != reactors_.end(); ++it) {
+    reactor = *it;
+    if (!strcmp(reactor->tissue()->name().c_str(), _tissueName.c_str()))
+      break;
+  }
+  return reactor;
+}
 
 Cell::Coordinates Simulation::LocationMove(Cell::Coordinates _loc, CellMembrane::Side _side) {
   Cell::Coordinates newLoc = _loc;
   switch (_side) {
-    case CellMembrane::north_ : newLoc.y += 1;
-    case CellMembrane::south_: newLoc.y -= 1;
-    case CellMembrane::east_: newLoc.x += 1;
-    case CellMembrane::west_: newLoc.x -= 1;
-    case CellMembrane::up_: newLoc.z += 1;
-    case CellMembrane::down_: newLoc.z -= 1;
+    case CellMembrane::north_ : newLoc.y += 1; break;
+    case CellMembrane::south_: newLoc.y -= 1; break;
+    case CellMembrane::east_: newLoc.x += 1; break;
+    case CellMembrane::west_: newLoc.x -= 1; break;
+    case CellMembrane::up_: newLoc.z += 1; break;
+    case CellMembrane::down_: newLoc.z -= 1; break;
     default : throw std::runtime_error("Invalid side");
   }
   return newLoc;
--- Simulation.h
+++ Simulation.h
@@ -27,18 +27,35 @@ public:
   void CloneCells(Fwk::String _tissueName, CellMembrane::Side _side);
   void AntibodyStrengthIs(Fwk::String _tissueName, Cell::Coordinates _loc,
                           CellMembrane::Side _side, AntibodyStrength _strength);
-
   TissueList *Tissues() { return &tissues_; }
 
-  /* Event Reactor */
   class TissueReactor : public Tissue::Notifiee {
   public:
+    typedef Fwk::Ptr<TissueReactor> Ptr;
     void onCellNew(Cell::Ptr _cell);
-    static TissueReactor::Ptr TissueReactorIs(Tissue::Ptr _tissue) {
-      return new TissueReactor(_tissue);
-    }
+    Tissue::Ptr tissue() { return tissue_; };
+
+    static TissueReactor::Ptr TissueReactorNew(Tissue::Ptr _tissue) {
+      TissueReactor::Ptr reactor = new TissueReactor(_tissue);
+      reactor->referencesDec(1);
+      return reactor;
+    };
+
+    U32 infected() { return infected_; };
+    U32 attempts() { return attempts_; };
+    U32 strengthDiff() { return strengthDiff_; };
+    U32 cytotoxicCount() { return cytotoxicCount_; };
+    U32 helperCount() { return helperCount_; };
+    U32 spread() { return spread_; };
+    U32 pathLength() { return pathLength_; };
+    void infectedInc(U32 val) { infected_ += val; };
+    void attemptsInc(U32 val) { attempts_ += val; };
+    void cytotoxicCountInc(U32 val) { cytotoxicCount_ += val; };
+    void helperCountInc(U32 val) { helperCount_ += val; };
+
   protected:
     TissueReactor(Tissue::Ptr _tissue) : Tissue::Notifiee() {
+      tissue_ = _tissue;
       notifierIs(_tissue);
     }
 
@@ -48,17 +65,21 @@ public:
     U32 cytotoxicCount_;
     U32 helperCount_;
     U32 spread_;
-    U32 pathLength;
+    U32 pathLength_;
+    Tissue::Ptr tissue_;
   };
+
+  typedef std::vector<TissueReactor::Ptr> ReactorList;
 
 
 protected:
   TissueList tissues_;
-  //std::vector<Tissue::Ptr> reactors_;
+  ReactorList reactors_;
 
   bool InfectedCellIs(Cell::Ptr _cell, CellMembrane::Side _side, 
                       AntibodyStrength _strength);
   Tissue::Ptr GetTissue(Fwk::String _tissueName);
+  TissueReactor::Ptr GetReactor(Fwk::String _tissueName);
   Cell::Coordinates LocationMove(Cell::Coordinates _loc, CellMembrane::Side _side);
 };
 
