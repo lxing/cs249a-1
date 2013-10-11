335835e233a43278a0e9b56325bd2abb3f312278
Lawrence Xing <lawrence.xing@gmail.com>
2013-10-11 14:39:56 -0700

    Finish simulation tests

--- unit-tests/SimulationTest.cpp
+++ unit-tests/SimulationTest.cpp
@@ -3,6 +3,7 @@
 
 #define TISSUE "Tissue1"
 #define MULTI "Tissue2"
+#define INFECTED "Tissue3"
 #define SIDE_MIN 0
 #define SIDE_MAX 6
 
@@ -30,6 +31,16 @@ protected:
     sim.cellIs(MULTI, Cell::cytotoxicCell_, Loc(0,0,0));
     sim.cellIs(MULTI, Cell::cytotoxicCell_, Loc(1,0,0));
     sim.cellIs(MULTI, Cell::helperCell_, Loc(0,1,0));
+
+    sim.tissueIs(INFECTED);
+    sim.cellIs(INFECTED, Cell::helperCell_, Loc(0,0,0));
+    sim.cellIs(INFECTED, Cell::helperCell_, Loc(1,0,1));
+    sim.cellIs(INFECTED, Cell::helperCell_, Loc(1,0,0));
+    sim.cellIs(INFECTED, Cell::cytotoxicCell_, Loc(2,0,0));
+    sim.cellIs(INFECTED, Cell::helperCell_, Loc(3,0,0));
+    sim.cellIs(INFECTED, Cell::helperCell_, Loc(-1,0,0));
+    sim.cellIs(INFECTED, Cell::helperCell_, Loc(-2,0,0));
+    sim.cellIs(INFECTED, Cell::helperCell_, Loc(-2,1,0));
   }
 
   Simulation sim;
@@ -97,6 +108,12 @@ TEST_F(SimulationTest, ClonedCellsAre) {
 
 TEST_F(SimulationTest, InfectionDel) {
   Tissue::Ptr tissue = sim.tissue(MULTI);  
+  Cell::Ptr cell = tissue->cell(Loc(1,0,0));
+  cell->healthIs(Cell::infected_);
+  
+  sim.infectionDel(MULTI);
+  Cell::Ptr deletedCell = tissue->cell(Loc(1,0,0));
+  ASSERT_EQ(deletedCell, NULL);
 }
 
 TEST_F(SimulationTest, CellCounts) {
@@ -109,5 +126,35 @@ TEST_F(SimulationTest, CellCounts) {
   sim.clonedCellsAre(MULTI, CellMembrane::east_);
   ASSERT_EQ(stats->cytotoxicCount(), 3);
   ASSERT_EQ(stats->helperCount(), 2);
+
+  /* Test counts after infect */
+  stats = sim.stats(INFECTED);
+  sim.infectionIs(INFECTED, Loc(0,0,0), CellMembrane::north_, 40);
+  ASSERT_EQ(stats->infected(), 6);
+
+  /* Test counts after infectionDel */
+  sim.infectionDel(INFECTED);
+  ASSERT_EQ(stats->infected(), 0);
+  ASSERT_EQ(stats->cytotoxicCount(), 1);
+  ASSERT_EQ(stats->helperCount(), 1);
+ 
+}
+
+TEST_F(SimulationTest, PerInfectionStats) {
+  Stats::Ptr stats = sim.stats(INFECTED);
+  sim.infectionIs(INFECTED, Loc(0,0,0), CellMembrane::north_, 40);
+  ASSERT_EQ(stats->strengthDiff(), 6 * (40 - 0) + 1 * (40 - 100));
+  ASSERT_EQ(stats->pathLength(), 4);
+  ASSERT_EQ(stats->spread(), (1 - (-2) + 1) * (1 - 0 + 1) * (1 - 0 + 1));
+
+  /* Try computing a bounding box that doesn't enclose (0,0,0) */ 
+  stats = sim.stats(MULTI);
+  sim.cellIs(MULTI, Cell::helperCell_, Loc(0,4,0));
+  sim.cellIs(MULTI, Cell::helperCell_, Loc(0,4,1));
+  sim.cellIs(MULTI, Cell::helperCell_, Loc(0,4,2));
+  sim.cellIs(MULTI, Cell::helperCell_, Loc(-1,4,2));
+  sim.cellIs(MULTI, Cell::helperCell_, Loc(-1,5,2));
+  sim.infectionIs(MULTI, Loc(0,4,0), CellMembrane::north_, 40);
+  ASSERT_EQ(stats->spread(), (0 - (-1) + 1) * (5 - 4 + 1) * (2 - 0 + 1));
 }
 
